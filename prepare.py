fc7537a70348581782cb598101e14c5270c0b773
stephen-c-perry <stephen.christian.perry@gmail.com>
2023-03-05 18:34:43 -0600

    finished exercises

--- prepare.py
+++ prepare.py
@@ -25,29 +25,18 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-'''
-Instructor's functions below for reference
-'''
-
-
 def basic_clean(string):
+
+    ''' 
+    Takes in string, makes it all lowercase, encoded in ascii all characters not ascii ignored.  
+    decoded in utf-8 all characters not utf-8 ignored.  remove special characters.  return cleaned string
     '''
-    This function takes in the original text.
-    The text is all lowercased, 
-    the text is encoded in ascii and any characters that are not ascii are ignored.
-    The text is then decoded in utf-8 and any characters that are not ascii are ignored
-    Additionally, special characters are all removed.
-    A clean article is then returned
-    '''
-    #lowercase
+  
     string = string.lower()
-    
-    #normalize
     string = unicodedata.normalize('NFKD', string)\
     .encode('ascii', 'ignore')\
     .decode('utf-8', 'ignore')
     
-    #remove special characters and replaces it with blank
     string = re.sub(r"[^a-z0-9'\s]", '', string)
     
     return string
@@ -55,14 +44,12 @@ def basic_clean(string):
 
 
 def tokenize(string):
+    
     '''
-    This function takes in a string
-    and returns the string as individual tokens put back into the string
+    Takes in string and returns it as individual tokens put back into the string
     '''
-    #create the tokenizer
-    tokenizer = nltk.tokenize.ToktokTokenizer()
 
-    #use the tokenizer
+    tokenizer = nltk.tokenize.ToktokTokenizer()
     string = tokenizer.tokenize(string, return_str = True)
 
     return string
@@ -70,17 +57,13 @@ def tokenize(string):
 
 
 def stem(string):
+
+    ''' 
+    takes in string, returns stem word joined back into the string
     '''
-    This function takes in text
-    and returns the stem word joined back into the text
-    '''
-    #create porter stemmer
+
     ps = nltk.porter.PorterStemmer()
-    
-    #use the stem, split string using each word
     stems = [ps.stem(word) for word in string.split()]
-    
-    #join stem word to string
     string = ' '.join(stems)
 
     return string
@@ -88,17 +71,9 @@ def stem(string):
 
 
 def lemmatize(string):
-    '''
-    This function takes in a string
-    and returns the lemmatized word joined back into the string
-    '''
-    #create the lemmatizer
+
     wnl = nltk.stem.WordNetLemmatizer()
-    
-    #look at the article 
     lemmas = [wnl.lemmatize(word) for word in string.split()]
-    
-    #join lemmatized words into article
     string = ' '.join(lemmas)
 
     return string
@@ -106,56 +81,35 @@ def lemmatize(string):
 
 
 def remove_stopwords(string, extra_words = [], exclude_words = []):
-    '''
-    This function takes in text, extra words and exclude words
-    and returns a list of text with stopword removed
-    '''
-    #create stopword list
+
     stopword_list = stopwords.words('english')
-    
-    #remove excluded words from list
     stopword_list = set(stopword_list) - set(exclude_words)
-    
-    #add the extra words to the list
     stopword_list = stopword_list.union(set(extra_words))
-    
-    #split the string into different words
     words = string.split()
-    
-    #create a list of words that are not in the list
     filtered_words = [word for word in words if word not in stopword_list]
-    
-    #join the words that are not stopwords (filtered words) back into the string
     string = ' '.join(filtered_words)
     
     return string
 
 
 
-################################ PREP ARTICLES ################################
 
-#take dataframe, specify the column, extra and exclude words
-def prep_article_data(df, column, extra_words=[], exclude_words=[]):
+
+
+def prep_data(df, column, extra_words=[], exclude_words=[]):
+
     '''
-    This function take in a df and the string name for a text column with 
-    option to pass lists for extra_words and exclude_words and
     returns a df with the text article title, original text, stemmed text,
     lemmatized text, cleaned, tokenized, & lemmatized text with stopwords removed.
     '''
-    #original text from content column
+
     df['original'] = df['content']
-    
-    #chain together clean, tokenize, remove stopwords
     df['clean'] = df[column].apply(basic_clean)\
                             .apply(tokenize)\
                             .apply(remove_stopwords, 
                                    extra_words=extra_words, 
                                    exclude_words=exclude_words)
-    
-    #chain clean, tokenize, stem, remove stopwords
     df['stemmed'] = df['clean'].apply(stem)
-    
-    #clean clean, tokenize, lemmatize, remove stopwords
     df['lemmatized'] = df['clean'].apply(lemmatize)
     
     return df[['title', 'original', 'clean', 'stemmed', 'lemmatized']]
